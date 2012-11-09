735681aacd3d2dacb94d924e8aab74af4427bb6c
Guangwei Jiang <Guangwei_Jiang@wistron.com>
2012-11-09 15:38:56 +0800

    first commit prog1_3.

---
+++ chap01/prog1_3/prog1_3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int
+main(void)
+{
+	int		c;
+
+	while ((c = getc(stdin)) != EOF)
+		if (putc(c, stdout) == EOF)
+			printf("output error\n");
+
+	if (ferror(stdin))
+		printf("input error\n");
+
+	exit(0);
+}
